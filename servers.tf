f6c1c14944d0471736f4df7d8079cfed5ea4c263
Pavan yandrapati <pavanyandrapati345@gmail.com>
2023-04-25 21:19:08 +0530

    frontend

--- servers.tf
+++ servers.tf
@@ -1,3 +1,11 @@
+data "aws_ami" "centos" {
+  executable_users = ["973714476881"]
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+}
+
+
+
 resource "aws_instance" "frontend" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
