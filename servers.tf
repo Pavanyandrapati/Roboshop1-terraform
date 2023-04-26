f36a666c74a8b810331b4e457048b54925a1f926
Pavan yandrapati <pavanyandrapati345@gmail.com>
2023-04-26 23:35:23 +0530

    frontend

--- servers.tf
+++ servers.tf
@@ -3,22 +3,24 @@ data "aws_ami" "centos" {
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
 }
+
 data "aws_security_group" "allow-all" {
-  Group = "allow-all"
+  name = "allow-all"
 }
+
 variable "instance_type" {
   default = "t3.small"
 }
 
 variable "components" {
-  default = ["frontend","mongodb","catalogue"]
+  default = [ "frontend", "mongodb", "catalogue" ]
 }
-resource "aws_instance" "instances" {
-  ami           = data.aws_ami.centos.image_id
- instance_type = var.instance_type
-  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
-  count = length(var.components)
 
+resource "aws_instance" "instance" {
+  count         = length(var.components)
+  ami           = data.aws_ami.centos.image_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
 
   tags = {
     Name = var.components[count.index]
