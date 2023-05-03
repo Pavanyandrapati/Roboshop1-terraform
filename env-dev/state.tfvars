9c75f7f26237b3cb26e7837e5b1de621f94d6472
Pavan yandrapati <pavanyandrapati345@gmail.com>
2023-05-03 23:15:09 +0530

    frontend

---
+++ env-dev/state.tfvars
@@ -0,0 +1,3 @@
+bucket = "roboshop1-terraform"
+key    = "roboshop/dev/terraform-tfstate"
+region = "us-east-1"
--- state.tf
+++ state.tf
@@ -1,7 +1,4 @@
 terraform {
   backend "s3" {
-    bucket = "Roboshop1-terraform"
-    key    = "roboshop/dev/terraform-tfstate"
-    region = "us-east-1"
   }
 }
