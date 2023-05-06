a02db702065c592e28071695d226e1ad31292635
Pavan yandrapati <pavanyandrapati345@gmail.com>
2023-05-06 11:29:43 +0530

    frontend

--- servers.tf
+++ servers.tf
@@ -1,44 +1,9 @@
-resource "aws_instance" "instance" {
+module "servers" {
   for_each = var.components
-  ami          = data.aws_ami.centos.image_id
+
+  source = "./module"
+  component_name = each.value["name"]
+  env = var.env
   instance_type = each.value["instance_type"]
-  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
-  tags = {
-    Name = each.value["name"]
-  }
+  password = lookup(each.value,"password","null")
 }
-
-
-resource "null_resource" "provisioner" {
-
-  depends_on = [aws_instance.instance,aws_route53_record.records]
-
-  for_each = var.components
-  provisioner "remote-exec" {
-
-
-    connection {
-      type     = "ssh"
-      user     = "centos"
-      password = "DevOps321"
-      host     = aws_instance.instance[each.value["name"]].private_ip
-   }
-
-    inline = [
-      "rm -rf Roboshop1",
-      "git clone https://github.com/Pavanyandrapati/Roboshop1",
-      "cd Roboshop1",
-      "sudo bash ${each.value["name"]}.sh ${lookup(each.value,"password","null")}"
-    ]
- }
-}
-
- resource "aws_route53_record" "records" {
-  for_each = var.components
-  zone_id = "Z08045122E2EQN1OR1WS6"
-  name    = "${each.value["name"]}-dev.pavan345.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.instance[each.value["name"]].private_ip]
-}
-
