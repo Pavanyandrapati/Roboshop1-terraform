ac1adfcad32f0cd703ba860bc2c388b412265b3f
Pavan yandrapati <pavanyandrapati345@gmail.com>
2023-05-21 23:15:41 +0530

    frontend

--- servers.tf
+++ servers.tf
@@ -1,21 +1,24 @@
 module "database-servers" {
   for_each = var.database_servers
 
-  source = "./module"
+  source         = "./module"
   component_name = each.value["name"]
-  env = var.env
-  instance_type = each.value["instance_type"]
-  password = lookup(each.value,"password","null")
-  provisioner = true
+  env            = var.env
+  instance_type  = each.value["instance_type"]
+  password       = lookup(each.value, "password", "null")
+  provisioner    = true
+  app_type       = "db"
 }
 
 module "app-servers" {
   depends_on = [module.database-servers]
-  for_each = var.app_servers
+  for_each   = var.app_servers
 
-  source = "./module"
+  source         = "./module"
   component_name = each.value["name"]
-  env = var.env
-  instance_type = each.value["instance_type"]
-  password = lookup(each.value,"password","null")
+  env            = var.env
+  instance_type  = each.value["instance_type"]
+  password       = lookup(each.value, "password", "null")
+  provisioner    = true
+  app_type       = "app"
 }
