c400be15f287df6a263b3cbf45e3a05adcadeed5
Pavan yandrapati <pavanyandrapati345@gmail.com>
2023-05-26 01:10:55 +0530

    frontend

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
 module "vpc" {
-   source = "git::https://github.com/Pavanyandrapati/tf-module-vpc.git"
+  source = "git::https://github.com/Pavanyandrapati/tf-module-vpc.git"
 
-  for_each = var.vpc
+  for_each   = var.vpc
   cidr_block = each.value["cidr_block"]
-  subnets = each.value["subnets"]
-  tags = local.tags
-  env = var.env
-
+  subnets    = each.value["subnets"]
+  tags       = local.tags
+  env        = var.env
 }
 
 
 module "web" {
   source = "git::https://github.com/Pavanyandrapati/tf-module-app.git"
-   for_each = var.app
-  instance_type = each.value["instance_type"]
-  subnet_id = element(lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null), 0)
 
+  for_each      = var.app
+  instance_type = each.value["instance_type"]
+  subnet_id     = element(lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null), 0)
 }
+
