5699dfa2514051ab191748d957a3998f38c4d245
Pavan yandrapati <pavanyandrapati345@gmail.com>
2023-05-27 14:43:09 +0530

    frontend

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ module "vpc" {
 #    allow_app_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_cidrs", null)
 #}
 
-module "vpc" {
+module "db" {
   source = "git::https://github.com/Pavanyandrapati/tf-module-docdb.git"
 
   for_each   = var.docdb
